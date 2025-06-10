e604e7d39ac04c5ccb1f5e9fa402f5658c9d110c
everywhere.z <weenyc31@163.com>
2025-06-10 12:53:48 +0800

    submit

--- src/main/java/com/yomahub/liteflowhelper/completion/LiteFlowElCompletionContributor.java
+++ src/main/java/com/yomahub/liteflowhelper/completion/LiteFlowElCompletionContributor.java
@@ -9,11 +9,15 @@ import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.psi.xml.XmlTokenType;
 import com.intellij.util.ProcessingContext;
+import com.yomahub.liteflowhelper.icon.LiteFlowIcons;
+import com.yomahub.liteflowhelper.service.LiteFlowCacheService;
+import com.yomahub.liteflowhelper.toolwindow.model.ChainInfo;
+import com.yomahub.liteflowhelper.toolwindow.model.LiteFlowNodeInfo;
 import com.yomahub.liteflowhelper.utils.LiteFlowXmlUtil;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * 为 LiteFlow EL 表达式提供关键字代码补全功能。
+ * 为 LiteFlow EL 表达式提供关键字、组件、子流程的代码补全功能。
  *
  * @author Bryan.Zhang
  */
@@ -21,7 +25,6 @@ public class LiteFlowElCompletionContributor extends CompletionContributor {
 
     public LiteFlowElCompletionContributor() {
         // 定义补全的触发位置：在名为 "chain" 的 XML 标签内部的文本区域
-        // [修复] 使用 XmlPatterns.xmlTag() 替代已废弃的 PlatformPatterns.xmlTag()
         extend(CompletionType.BASIC,
                 PlatformPatterns.psiElement(XmlTokenType.XML_DATA_CHARACTERS)
                         .inside(XmlPatterns.xmlTag().withLocalName("chain")),
@@ -35,7 +38,6 @@ public class LiteFlowElCompletionContributor extends CompletionContributor {
                         PsiElement parent = position.getParent();
 
                         // 向上查找，确保我们确实在 <chain> 标签内部
-                        // [修复] 使用 getLocalName 替代 getName 方法
                         if (!(parent instanceof XmlTag) || !"chain".equals(((XmlTag) parent).getLocalName())) {
                             parent = parent.getParent();
                             if (!(parent instanceof XmlTag) || !"chain".equals(((XmlTag) parent).getLocalName())) {
@@ -48,14 +50,48 @@ public class LiteFlowElCompletionContributor extends CompletionContributor {
                             return;
                         }
 
-                        // 添加所有 EL 关键字到补全列表
+                        // 1. 添加所有 EL 关键字到补全列表
                         for (String keyword : LiteFlowXmlUtil.getElKeywords()) {
                             resultSet.addElement(
                                     LookupElementBuilder.create(keyword)
+                                            .withIcon(LiteFlowIcons.EL_KEYWORD_ICON) // 设置关键字图标
                                             .withBoldness(true) // 在补全列表中加粗显示
-                                            .withTypeText("LiteFlow EL") // 在右侧显示类型提示
+                                            .withTypeText("LiteFlow EL", true) // 在右侧显示类型提示
                             );
                         }
+
+                        // 获取缓存服务
+                        LiteFlowCacheService cacheService = LiteFlowCacheService.getInstance(parameters.getEditor().getProject());
+                        if(cacheService == null){
+                            return;
+                        }
+
+                        // 2. 添加所有已缓存的组件 (Nodes) 到补全列表
+                        for(LiteFlowNodeInfo node : cacheService.getCachedNodes()){
+                            LookupElementBuilder element = LookupElementBuilder.create(node.getNodeId())
+                                    .withIcon(node.getType().getIcon()) // 根据组件类型设置图标
+                                    .withBoldness(true)
+                                    .withTailText(" (" + node.getFileName() + ")", true) // 显示所在文件名
+                                    .withTypeText(node.getType().getDescription(), true); // 显示组件类型描述
+                            resultSet.addElement(element);
+                        }
+
+
+                        // 3. 添加所有已缓存的子流程 (Chains) 到补全列表
+                        for(ChainInfo chain : cacheService.getCachedChains()){
+                            // 避免将当前正在编辑的chain自身也添加到补全列表
+                            if(parent.isEquivalentTo(chain.getPsiFile().findElementAt(chain.getOffset()).getParent())){
+                                continue;
+                            }
+
+                            LookupElementBuilder element = LookupElementBuilder.create(chain.getName())
+                                    .withIcon(LiteFlowIcons.CHAIN_ICON) // 设置Chain图标
+                                    .withBoldness(true)
+                                    .withTailText(" (" + chain.getFileName() + ")", true) // 显示所在文件名
+                                    .withTypeText("Chain", true); // 类型为Chain
+                            resultSet.addElement(element);
+                        }
+
                     }
                 }
         );
---
+++ src/main/java/com/yomahub/liteflowhelper/icon/LiteFlowIcons.java
@@ -0,0 +1,33 @@
+package com.yomahub.liteflowhelper.icon;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * 插件中使用的图标常量
+ *
+ * @author Bryan.Zhang
+ */
+public class LiteFlowIcons {
+
+    /**
+     * Chain 图标
+     */
+    public static final Icon CHAIN_ICON = IconLoader.getIcon("/icons/chain.svg", LiteFlowIcons.class);
+
+    /**
+     * 普通组件图标
+     */
+    public static final Icon COMMON_COMPONENT_ICON = IconLoader.getIcon("/icons/common.svg", LiteFlowIcons.class);
+
+    /**
+     * 脚本组件图标
+     */
+    public static final Icon SCRIPT_COMPONENT_ICON = IconLoader.getIcon("/icons/script_common.svg", LiteFlowIcons.class);
+
+    /**
+     * LiteFlow EL 关键字图标
+     */
+    public static final Icon EL_KEYWORD_ICON = IconLoader.getIcon("/icons/el_keyword.svg", LiteFlowIcons.class);
+}
